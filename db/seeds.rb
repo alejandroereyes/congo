a0469ebe83f3fc52a048996d97c7ae53ea864631
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-25 18:21:33 -0500

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times do
+ item_one =  Item.create(title: Faker::Commerce.product_name, cost: Array(1..100).sample, description: Faker::Lorem.paragraph(2),
+              inventory: Array(100..1000).sample, img_url: ["http://www.saberforge.com/images/stories/virtuemart/product/9758.jpg", "http://www.atomicmoo.com/wp-content/uploads/2013/03/Etvideogamecover.jpg"].sample)
+ item_two =  Item.create(title: Faker::Commerce.product_name, cost: Array(1..100).sample, description: Faker::Lorem.paragraph(2),
+                         inventory: Array(100..1000).sample, img_url: ["http://allansnewtoysforchristmas.com/wp-content/uploads/2012/08/12.jpg","http://i.kinja-img.com/gawker-media/image/upload/s--l2G1Vj9p--/c_fit,fl_progressive,q_80,w_636/194ibbm3go1rqpng.png"].sample)
+
+  user = User.new
+  user.name = Faker::Name.first_name
+  user.email = Faker::Internet.free_email("#{user.name}")
+  user.password = user.name
+  user.password_confirmation = user.name
+  user.save
+
+  order = Order.create(user_id: user.id, completed: [true, false].sample)
+
+  OrderItem.create(order_id: order.id, item_id: item_one.id, quantity: Array(1..5).sample)
+
+  if user.name.match(/^[aeiou]/i)
+    OrderItem.create(order_id: order.id, item_id: item_two.id, quantity: Array(1..5).sample)
+  end
+end
+
