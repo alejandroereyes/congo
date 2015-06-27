8392018caf01235380f7a8b26984d9e2fd0d1c09
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-27 11:34:01 -0500

    added a item show page, very basic, just showing item info

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,12 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def show
+    begin
+      @item = Item.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => error
+      redirect_to :back, flash[:alert] = 'Record not Found', status: 404
+    end
+  end
 end
