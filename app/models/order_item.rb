57565b1e6dea9a7f752724014b24780a32326d0c
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-28 14:24:43 -0500

    order show for admin info done, shows id cus name if paid and total,will add link for each order show page

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,13 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
+
+  def self.total_due(order_id)
+    amount_due = 0
+    items_from_table = OrderItem.select('*').where(order_id: order_id)
+    items_from_table.each do|item|
+      amount_due += (item.item.cost * item.quantity)
+    end
+    amount_due
+  end
 end
