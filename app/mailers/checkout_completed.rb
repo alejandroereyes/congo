4605fdf22c50c3dd73d751c0194ea878e5fe4e9a
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-28 17:45:29 -0500

    generated checkout complete mailer

---
+++ app/mailers/checkout_completed.rb
@@ -0,0 +1,2 @@
+class CheckoutCompleted < ApplicationMailer
+end
---
+++ test/mailers/previews/checkout_completed_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/checkout_completed
+class CheckoutCompletedPreview < ActionMailer::Preview
+
+end
