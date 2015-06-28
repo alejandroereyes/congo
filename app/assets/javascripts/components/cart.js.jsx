8a5c93b3c592b9d8850fc109f9b3c218f0bbfc3b
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-28 16:14:18 -0500

    able to send data to react cart comp, need to format info

--- app/assets/javascripts/components/cart.js.jsx
+++ app/assets/javascripts/components/cart.js.jsx
@@ -3,16 +3,24 @@
 var Cart = React.createClass({
     getInitialState: function() {
         return {
-            why: ""
-        }
+            userName: this.props.name,
+            orderNum: this.props.orderId,
+            allItems: this.props.items
+        };
     },
 
 
    render: function () {
        return (
-           <div>
-            <p>Hello {this.props.name}</p>
+           <div className="side-cart">
+               <p> Shopping Cart</p>
+               <p>Hello {this.state.userName}</p>
+               <p>Order #{this.state.orderNum}</p>
+               <p>Items {this.state.allItems}</p>
            </div>
        )
-   }
+   },
+
+
+
 });
