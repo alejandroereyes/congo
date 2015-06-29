6a4566340960b0fb0cc5f60605bc2f59118d456d
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-28 19:57:27 -0500

    updating models and seed to work with heroku

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20150625211602) do
   create_table "users", force: :cascade do |t|
     t.string   "name",                            null: false
     t.string   "email",                           null: false
-    t.string   "password_digest",                 null: false
+    t.string   "password_digest"
     t.boolean  "admin",           default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,10 @@
  item_two =  Item.create(title: Faker::Commerce.product_name, cost: Array(1..100).sample, description: Faker::Lorem.paragraph(2),
                          inventory: Array(100..1000).sample, img_url: ["http://allansnewtoysforchristmas.com/wp-content/uploads/2012/08/12.jpg","http://i.kinja-img.com/gawker-media/image/upload/s--l2G1Vj9p--/c_fit,fl_progressive,q_80,w_636/194ibbm3go1rqpng.png"].sample)
 
-  user = User.new
-  user.name = Faker::Name.first_name
-  user.email = Faker::Internet.free_email("#{user.name}")
-  user.password = user.name
-  user.password_confirmation = "word"
-  user.save
+  user = User.create(name: Faker::Name.first_name, email: Faker::Interet.email, password: 'word',
+                     password_confirmation: 'word', admin: [true, false].sample)
 
-  order = Order.create(user_id: user.id, completed: [true, false].sample)
+  order = Order.create(user_id: user[:id], completed: [true, false].sample)
 
   OrderItem.create(order_id: order.id, item_id: item_one.id, quantity: Array(1..5).sample)
 
