1028b8a85d9608467f18172ff1235ddc1e80e9f6
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-28 20:02:19 -0500

    updaing emial on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
  item_two =  Item.create(title: Faker::Commerce.product_name, cost: Array(1..100).sample, description: Faker::Lorem.paragraph(2),
                          inventory: Array(100..1000).sample, img_url: ["http://allansnewtoysforchristmas.com/wp-content/uploads/2012/08/12.jpg","http://i.kinja-img.com/gawker-media/image/upload/s--l2G1Vj9p--/c_fit,fl_progressive,q_80,w_636/194ibbm3go1rqpng.png"].sample)
 
-  user = User.create(name: Faker::Name.first_name, email: Faker::Interet.email, password: 'word',
+  user = User.create(name: Faker::Name.first_name, email: 'test@example.com', password: 'word',
                      password_confirmation: 'word', admin: [true, false].sample)
 
   order = Order.create(user_id: user[:id], completed: [true, false].sample)
